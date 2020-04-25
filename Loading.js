eb15dcc53c17326d17e6e0a7aa8d17d2e77226ff
MVZakharchenko <mvzakharchenko@beeline.ru>
2020-04-26 00:54:45 +0300

    Setup basic authentification

---
+++ Loading.js
@@ -0,0 +1,28 @@
+// Loading.js
+import React from 'react';
+import auth from '@react-native-firebase/auth';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+export default class Loading extends React.Component {
+
+  componentDidMount() {
+    auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'Login')
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
---
+++ Main.js
@@ -0,0 +1,30 @@
+// Main.js
+import React from 'react'
+import auth from '@react-native-firebase/auth';
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+export default class Main extends React.Component {
+
+state = { currentUser: null }
+  componentDidMount() {
+    const { currentUser } = auth()
+    this.setState({ currentUser })
+}
+
+render() {
+    const { currentUser } = this.state
+return (
+      <View style={styles.container}>
+        <Text>
+          Hi {currentUser && currentUser.email}!
+        </Text>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
