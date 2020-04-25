b1790067a9ca5776b5f64a89fb3d6e47a84a059c
MVZakharchenko <mvzakharchenko@beeline.ru>
2020-04-26 00:53:53 +0300

    Setup basic authentification

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
-/**
- * Sample React Native App with Firebase
- * https://github.com/invertase/react-native-firebase
- *
- * @format
- * @flow
- */
-
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+import { createSwitchNavigator,createAppContainer  } from 'react-navigation';
 import firebase from '@react-native-firebase/app';
+import Loading from './Loading'
+import Login from './Login'
+import Main from './Main'
 
 // TODO(you): import any additional firebase services that you require for your app, e.g for auth:
 //    1) install the npm package: `yarn add @react-native-firebase/auth@alpha` - you do not need to
@@ -18,50 +14,13 @@ import firebase from '@react-native-firebase/app';
 //    3) import the package here in your JavaScript code: `import '@react-native-firebase/auth';`
 //    4) The Firebase Auth service is now available to use here: `firebase.auth().currentUser`
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
-});
-
-const firebaseCredentials = Platform.select({
-  ios: 'https://invertase.link/firebase-ios',
-  android: 'https://invertase.link/firebase-android',
-});
-
-type Props = {};
-
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native + Firebase!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        {!firebase.apps.length && (
-          <Text style={styles.instructions}>
-            {`\nYou currently have no Firebase apps registered, this most likely means you've not downloaded your project credentials. Visit the link below to learn more. \n\n ${firebaseCredentials}`}
-          </Text>
-        )}
-      </View>
-    );
-  }
+export default createAppContainer(createSwitchNavigator(
+{
+Loading,
+Login,
+Main
+},
+{
+initialRouteName: 'Loading'
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+));
